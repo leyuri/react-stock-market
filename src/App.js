54d612ff9fff915a00ad15a80a6a71b7c047f59c
leyuri <leyuri97@gmail.com>
2020-05-11 20:17:19 +0900

    Add List UI & Show Companies through Redux Store

--- src/App.js
+++ src/App.js
@@ -7,15 +7,24 @@ import {
   Link
 } from "react-router-dom";
 
+import { makeStyles } from '@material-ui/core/styles';
+
 import AboutPage from "./pages/AboutPage";
 import HelpPage from "./pages/HelpPage";
 import CompanyListPage from "./pages/CompanyListPage";
 import MainPage from "./pages/MainPage";
 
+const useStyles = makeStyles((theme) => ({
+  // theme라는게 넘어오면 테마에 기본적인 space들이 정해져 있다. 
+  app: {
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   return (
     <Router>
-      <div className="App">
+       <div className={classes.app}>
           <Switch>
             <Route path="/about">
               <AboutPage />
--- src/pages/CompanyListPage.js
+++ src/pages/CompanyListPage.js
@@ -1,21 +1,55 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
 import AppBar from "../components/AppBar";
 import BottomNav from "../components/BottomNav";
 
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import ImageIcon from '@material-ui/icons/Image';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+}));
+
+// props으로 받는 것
+const CompanyItem = ({company}) => {
+    const { description, symbol } = company;
+    return (
+    <ListItem>
+        <ListItemText 
+            primary={symbol} 
+            secondary={description} 
+        />
+    </ListItem>
+    )
+}
+
 const CompanyListPage = () => {
+    const classes = useStyles();
+    const companies = useSelector(state => state.companies)
+    // useSelector hook을 통하여 redux store의 state에 접근할 수 있다. 
+    // companies에 접근!
+
     return (
         <div>
             <AppBar/>
-            CompanyListPage
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            test
-            <br/><br/>
+            <List className={classes.root}>
+                {/* Hooks에서 selector를 가지고 redux에 있는 것 중 어떤 것을 props로 가져오면 된다 .*/}
+                {companies.map(company => <CompanyItem company={company}/>)}
+            </List>
             <BottomNav/>
         </div>
     )
